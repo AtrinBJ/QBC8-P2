499431bef6c249f0ca2214e3cb7bfeb6a8d41152
Mostafa Safdari <mostafasafdari1999@gmail.com>
2025-02-20 09:14:00 +0330

    add logout user

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for
-from flask_login import LoginManager,UserMixin,login_user
+from flask_login import LoginManager,UserMixin,login_user,logout_user
 from dotenv import load_dotenv
 import os
 
@@ -37,7 +37,12 @@ def signup():
 @app.route('/accounts/login') 
 def login():
     login_user()
-    return "Login Page"  
+    return "Login Page"
+
+@app.route('/accounts/logout')  
+def logout():
+    logout_user()
+    return redirect(url_for("login"))   
 
 if __name__ == "__main__":
     app.run()
